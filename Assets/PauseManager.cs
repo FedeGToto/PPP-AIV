77b7d89e8f02acfa689e7f7d97ecf705cdee16a3
TheMagicGamer99 <78040296+TheMagicGamer99@users.noreply.github.com>
2021-04-29 11:33:09 +0200

    pause manager test 2

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PauseManager : MonoBehaviour, IPointerClickHandler
+public class PauseManager : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
     [SerializeField] PlayerInputs player;
     bool isPause;
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        isPause = !isPause;
+        player.SetPause(isPause);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
     {
         isPause = !isPause;
         player.SetPause(isPause);

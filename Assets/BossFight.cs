fbf9feeb3c25b9ec04158efdf90aea4ce29930a3
TheMagicGamer99 <78040296+TheMagicGamer99@users.noreply.github.com>
2021-05-01 15:28:35 +0200

    level 5

--- Assets/BossFight.cs
+++ Assets/BossFight.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossFight : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerInputs p;
         collision.gameObject.TryGetComponent(out p);
@@ -12,7 +14,7 @@ public class BossFight : MonoBehaviour
         {
             if (p.GetKey())
             {
-
+                SceneManager.LoadScene("LevelSelect");
             }
         }
     }

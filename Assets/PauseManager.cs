34eeb4d3cda02ac67d168b0f7fb8e3676679d314
TheMagicGamer99 <78040296+TheMagicGamer99@users.noreply.github.com>
2021-05-02 19:24:00 +0200

    fix lvl pause

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -6,16 +6,24 @@ public class PauseManager : MonoBehaviour
     [SerializeField] PlayerInputs[] player;
     [SerializeField] int costForPause;
     bool isPause;
+    bool[] scenePaused;
+
+    private void Start()
+    {
+        scenePaused = new bool[player.Length];
+    }
 
     public void PauseOneScene()
     {
-        isPause = !isPause;
-        int p = -1;
-        do{
-            p = Random.Range(0, player.Length);
-        } 
-        while (player[p].IsPaused());
-        if (Inventory.instance.Remove(costForPause))
-            player[p].SetPause();
+        for (int i = 0; i < player.Length; i++)
+        {
+            if (!scenePaused[i])
+            {
+                scenePaused[i] = true;
+                player[i].SetPause();
+                Inventory.instance.Remove(costForPause);
+                return;
+            }
+        }
     }
 }
